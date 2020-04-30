c28ddf7bb422de0e21886c7fc073d0723368e347
ocfranz <franzjoseo@gmail.com>
2020-04-29 22:42:26 -0500

    Fixing signup errors

--- routes/users.js
+++ routes/users.js
@@ -10,32 +10,38 @@ router.post('/signup', async (req, res) =>{
     try{
         let userExists = await User.findOne({ email : email});
         if(userExists){
-            res.status(400).json({message : "Email Already Exists"});
+            return res.status(400).send({message : "Email Already Exists"});
         }else{
-            const newUser = new User({username: username, email : email , password : password, name : name});
-            newUser.save()
-                .then( data =>{
-    
-                    res.status(200).json({ id : data.id , username : data.username, password : data.password});
-                    /*
-                    const payload = {
-                        user: { id: data.id}
-                    };
-                    console.log('payload', payload);
+            let usernameExists = await User.findOne({ username : username});
+            if(usernameExists){
+                return res.status(400).send({message : "Username has already been taken"});
+            }else{
+                const newUser = new User({username: username, email : email , password : password, name : name});
+                newUser.save()
+                    .then( data =>{
+                        return res.status(200).send({ id : data.id , username : data.username, password : data.password});
+                        /*
+                        const payload = {
+                            user: { id: data.id}
+                        };
+                        console.log('payload', payload);
+                
+                        jwt.sign(payload,"randomString", {expiresIn: 10000},(err, token) => {
+                            if (err){ 
+                                throw err;
+                            }else{
+                                res.status(200).json({token});
+                            }
+                        });*/
+                    })
+                    .catch( err => {throw err});
+            }
+
             
-                    jwt.sign(payload,"randomString", {expiresIn: 10000},(err, token) => {
-                        if (err){ 
-                            throw err;
-                        }else{
-                            res.status(200).json({token});
-                        }
-                    });*/
-                })
-                .catch( err => {throw err});
-        }v
+        }
         
     }catch (err){
-        res.status(500).json(err);
+        res.status(500).send(err);
     }
 })
 
@@ -45,19 +51,18 @@ router.post('/login', async (req, res) =>{
         let user = await User.findOne({username});
         console.log(user);
         if(!user){
-            return res.status(400).json({message : "El usuario no existe"});
+            return res.status(400).send({message : "User does not exist"});
         }else{
             let isMatch = await bcrypt.compare(password, user.password);
             console.log(isMatch);
             if(!isMatch){
-                return res.status(400).json({message : "Password incorrecto"});
+                return res.status(400).send({message : "Wrong password"});
             }else{
-                return res.status(400).json({auth: true});
+                return res.status(400).send({auth: true});
             }
         }
     }catch (err){
-
-
+        res.status(500).send(err);
     }
 });
 
